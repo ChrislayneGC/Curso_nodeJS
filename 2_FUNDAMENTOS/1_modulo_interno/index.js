50ed3b4f8b64596cbbccb89887dc0e7a50c5e5e7
chrislaynegc <dev.chrislayne@gmail.com>
2023-01-23 17:41:24 -0300

    Modulo interno

---
+++ 2_FUNDAMENTOS/1_modulo_interno/index.js
@@ -0,0 +1,8 @@
+// Conceito: Cria um Arquivo, Exporta oque eu preciso, 
+//Importo com o Require, e executo. 
+
+const meuModulo = require('./meu_modulo')
+const soma = meuModulo.soma
+
+soma (2,3)
+soma (5,10)
