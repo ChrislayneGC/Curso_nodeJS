596ddb4ba9fb6667f56da8aba4c41374bbf1d32a
chrislaynegc <dev.chrislayne@gmail.com>
2023-01-26 16:06:30 -0300

    Explorando console - Template Strings

---
+++ 2_FUNDAMENTOS/7_explorando_console/index.js
@@ -0,0 +1,16 @@
+// Mais de um valor
+const x = 10
+const y = 'Rodolpho'
+const z = [1, 2]
+
+ console.log(x, y, z)
+
+
+// Contagem de Impressoes (Looks)
+console.count(`O valor de x e: ${x}, contagem!`)
+console.count(`O valor de x e: ${x}, contagem!`)
+console.count(`O valor de x e: ${x}, contagem!`)
+console.count(`O valor de x e: ${x}, contagem!`) // ${} template strings
+
+// Variavell entre String
+console.log("O nome é %s, ele é programador", y) // %s interpolador
